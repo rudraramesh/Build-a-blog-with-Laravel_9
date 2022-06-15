726463f809b25f35a7618a3c74d4cf397e909955
rubenbaduwal <baduwalramesh98@gmail.com>
2022-06-15 14:05:39 +0545

    create user model,migration and 'user register system' create

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserM;
+use Hash;
 
 class AuthController extends Controller
 {
@@ -11,5 +13,25 @@ class AuthController extends Controller
     }
     public function getRegister(){
         return view('auth.register');
+    }
+
+    public function postRegister(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:user_m_s',
+            'password'=>'required|min:6|max:20'
+        ]);
+        $user = new UserM();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        if($request){
+            return back()->with('success','you have registered successfully.');
+        }else{
+            return back()->with('error','something wrong');
+        }
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,6 +6,17 @@
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
         {!! Form::open() !!}
+        
+        @if(Session::has('success'))
+        <div class="alert alert-success">
+          {{Session::get('success')}}
+        </div>
+        @endif
+        @if(Session::has('error'))
+        <div class="alert alert-danger">
+          {{Session::get('error')}}
+        </div>
+        @endif
         {{ Form::label('name','Name:') }}
         {{ Form::text('name',null, ['class'=>'form-control']) }}
 
@@ -15,8 +26,8 @@
         {{ Form::label('password','Password:') }}
         {{ Form::password('password', ['class'=>'form-control']) }}
         
-        {{ Form::label('password_confirmation','Confirm Password:') }}
-        {{ Form::password('password_confirmation', ['class'=>'form-control']) }}
+        {{-- {{ Form::label('password_confirmation','Confirm Password:') }}
+        {{ Form::password('password_confirmation', ['class'=>'form-control']) }} --}}
       <br><br>
 
         {{ Form::submit('Login',['class'=>'btn btn-success w-100']) }}
